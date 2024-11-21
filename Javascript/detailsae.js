21e7a40ef0762596bb0d2b0b37d5b767f6245ce6
JoelHH68 <joelhauptmann@gmail.com>
2024-11-21 11:52:30 +0100

    J'ai bien avancé
    le titre est explicite !!

---
+++ Javascript/detailsae.js
@@ -0,0 +1,5 @@
+let param = new URLSearchParams(location.search);
+let Numero = param.get('Num_SAE');
+
+let Num_SAE = SAE.titre
+let semestre = SAE.
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,16 +1,16 @@
 
-let cles=Object.keys(SAE);
+let Num_SAE=Object.keys(SAE);
 
- for (let i = 0; i<cles.length; i++) 
+ for (let i = 0; i<Num_SAE.length; i++) 
     { 
-     document.querySelector(".content").innerHTML += "<div class='cases'><div class='num_sae'></div><div class='titre_sae'></div><div class='competences_sae'></div></div>"
+     document.querySelector(".content").innerHTML += "<a href='../detailsea.html?Num_SAE='"+Num_SAE+ "><div class='cases'><div class='num_sae'></div><div class='titre_sae'></div><div class='competences_sae'></div></div></a>"
     }
 
-
+console.log(Num_SAE)
 const boites = document.querySelectorAll(".cases");
 
 boites.forEach((cases, indice)=>{
-   const cle = cles[indice];
+   const cle = Num_SAE[indice];
    const sae = SAE[cle];
 
    if(sae){
