56d4243a6f5a6d41261e2f2e4338ad04c9e83847
JoelHH68 <joelhauptmann@gmail.com>
2024-11-24 11:19:16 +0100

    Avancement js
    J'ai bien avancer sur le js mais j'ai un problème au niveau du lien entre le sommaire et les détails

--- Javascript/detailsae.js
+++ Javascript/detailsae.js
@@ -1,5 +1,14 @@
-let param = new URLSearchParams(location.search);
-let Numero = param.get('Num_SAE');
+let params = new URLSearchParams(window.location.search);
+let Num_SAE = params.get("sae");
 
-let Num_SAE = SAE.titre
-let semestre = SAE.
+let titre_sae = SAE.titre;
+let semestre = SAE.semestre;
+let description = SAE.description;
+let AC = SAE.AC;
+let ressources = SAE.ressources;
+
+document.querySelector(".num_SAE").innerHTML = titre_sae;
+document.querySelector(".semestre").innerHTML = semestre;
+document.querySelector(".description").innerHTML = description;
+document.querySelector(".AC").innerHTML = AC;
+document.querySelector(".ressources").innerHTML = ressources;
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,15 +1,22 @@
-
 let Num_SAE=Object.keys(SAE);
 
  for (let i = 0; i<Num_SAE.length; i++) 
     { 
-     document.querySelector(".content").innerHTML += "<a href='../detailsea.html?Num_SAE='"+Num_SAE+ "><div class='cases'><div class='num_sae'></div><div class='titre_sae'></div><div class='competences_sae'></div></div></a>"
+     const cle = Num_SAE[i]; 
+     document.querySelector(".content").innerHTML += `
+     <a href='../detailsea.html?Num_SAE=${cle}'>
+         <div class='cases'>
+            <div class='num_sae'></div>
+            <div class='titre_sae'></div>
+            <div class='competences_sae'></div> 
+         </div>
+      </a>`;
     }
 
 console.log(Num_SAE)
 const boites = document.querySelectorAll(".cases");
 
-boites.forEach((cases, indice)=>{
+boites.forEach(function(cases, indice){
    const cle = Num_SAE[indice];
    const sae = SAE[cle];
 
@@ -17,7 +24,6 @@ boites.forEach((cases, indice)=>{
       cases.querySelector(".num_sae").innerHTML = cle;
       cases.querySelector(".titre_sae").innerHTML = sae["titre"];
       cases.querySelector(".competences_sae").innerHTML = sae["compétences"];
-
    }
 
 });
