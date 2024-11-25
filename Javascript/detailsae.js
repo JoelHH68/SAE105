7cd220a62a21c58142ee31f584300df3aa9dfe48
JoelHH68 <joelhauptmann@gmail.com>
2024-11-25 11:53:01 +0100

    Les liens fonctionnes
    Les liens fonctionnes enfin.

--- Javascript/detailsae.js
+++ Javascript/detailsae.js
@@ -1,14 +1,12 @@
-let params = new URLSearchParams(window.location.search);
-let Num_SAE = params.get("sae");
+let param = new URLSearchParams(location.search);
+let Num_SAE = param.get("Num_SAE");
 
-let titre_sae = SAE.titre;
-let semestre = SAE.semestre;
-let description = SAE.description;
-let AC = SAE.AC;
-let ressources = SAE.ressources;
 
-document.querySelector(".num_SAE").innerHTML = titre_sae;
-document.querySelector(".semestre").innerHTML = semestre;
-document.querySelector(".description").innerHTML = description;
-document.querySelector(".AC").innerHTML = AC;
-document.querySelector(".ressources").innerHTML = ressources;
+
+
+
+document.querySelector(".num_SAE").innerHTML = SAE.titre;
+document.querySelector(".semestre").innerHTML = SAE.semestre;
+document.querySelector(".description").innerHTML = SAE.description;
+document.querySelector(".AC").innerHTML = SAE.AC;
+document.querySelector(".ressources").innerHTML = SAE.ressources;
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,17 +1,19 @@
 let Num_SAE=Object.keys(SAE);
 
+let content_HTML =""
+
  for (let i = 0; i<Num_SAE.length; i++) 
-    { 
-     const cle = Num_SAE[i]; 
-     document.querySelector(".content").innerHTML += `
-     <a href='../detailsea.html?Num_SAE=${cle}'>
-         <div class='cases'>
-            <div class='num_sae'></div>
-            <div class='titre_sae'></div>
-            <div class='competences_sae'></div> 
+    {  
+     content_HTML += `
+     <a href="../detailsae.html?Num_SAE=${Num_SAE[i]}">
+         <div class="cases">
+            <div class="num_sae"></div>
+            <div class="titre_sae"></div>
+            <div class="competences_sae"></div> 
          </div>
       </a>`;
     }
+    document.querySelector(".content").innerHTML += content_HTML
 
 console.log(Num_SAE)
 const boites = document.querySelectorAll(".cases");
