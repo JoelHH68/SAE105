88f1c2984a0d1f16713cab0a8eaf8abe4d98aa2e
JoelHH68 <joelhauptmann@gmail.com>
2024-12-09 15:13:01 +0100

    Style de la dernière page
    J'ai avancé dans le style de la dernière page et aussi sur d'autres petits éléments qui avait besoin d'amélioration

--- Javascript/detailsae.js
+++ Javascript/detailsae.js
@@ -5,13 +5,13 @@ let Num_SAE = param.get("Num_SAE");
 if (SAE[Num_SAE]) {
     const sae = SAE[Num_SAE];
 
-    document.querySelector("h1").innerHTML = "Voici la "+ Num_SAE;
+    document.querySelector("h1").innerHTML = "Voici la " + Num_SAE;
     document.querySelector(".titre").innerHTML = sae.titre;
-    document.querySelector(".semestre").innerHTML = "Semestre : " + sae.semestre;
+    document.querySelector(".semestre").innerHTML = sae.semestre;
     document.querySelector(".description").innerHTML = sae.description;
 
     let Liste_compétences = "";
-    for (let compétence in sae.compétences){
+    for (let compétence in sae.compétences) {
         Liste_compétences += `<div>${sae.compétences[compétence]}</div>`;
     }
     document.querySelector(".competences").innerHTML = Liste_compétences;
@@ -23,8 +23,8 @@ if (SAE[Num_SAE]) {
     document.querySelector(".AC").innerHTML = Liste_AC;
 
     let Liste_ressources = "";
-    for(let ressource in sae.ressources){
+    for (let ressource in sae.ressources) {
         Liste_ressources += `<div>${ressource} : ${sae.ressources[ressource]}</div>`;
     };
-    document.querySelector(".ressources").innerHTML = Liste_ressources;  
+    document.querySelector(".ressources").innerHTML = Liste_ressources;
 };
