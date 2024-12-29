c45d5da65bce72327a3badd1a093ad7c77df86ed
JoelHH68 <joelhauptmann@gmail.com>
2024-12-29 10:57:02 +0100

    Commentaire
    Ajout des commentaires pour les css et les JS

--- Javascript/detailsae.js
+++ Javascript/detailsae.js
@@ -1,29 +1,36 @@
+// Récupère les paramètres de l'URL pour extraire la valeur associée à "Num_SAE"
 let param = new URLSearchParams(location.search);
+//Extrait la valeur de "Num_SAE" depuis l'url
 let Num_SAE = param.get("Num_SAE");
 
-
+// Vérification de si l'objet contient une entrée qui corrrespond au numéro de la SAE extrait avant 
 if (SAE[Num_SAE]) {
+    //Récupère les données de la SAE
     const sae = SAE[Num_SAE];
 
+    //Mise à jour du titre avec le nom de la bonne SAE
     document.querySelector("h1").innerHTML = "Voici la " + Num_SAE;
+    //Insère les informations spécifiques de la SAE dans les éléments correspondants
     document.querySelector(".titre").innerHTML = sae.titre;
     document.querySelector(".semestre").innerHTML = sae.semestre;
     document.querySelector(".description").innerHTML = sae.description;
 
-    console.log(Num_SAE)
 
+    //Génère une liste des Compétences associées à la SAE
     let Liste_compétences = "";
     for (let compétence in sae.compétences) {
         Liste_compétences += `<div>${sae.compétences[compétence]}</div>`;
     }
     document.querySelector(".competences").innerHTML = Liste_compétences;
 
+    //Génère une liste des AC associées à la SAE
     let Liste_AC = "";
     for (let ac in sae.AC) {
         Liste_AC += `<div>${ac} : ${sae.AC[ac]}</div>`;                                                                
     }
     document.querySelector(".AC").innerHTML = `<a href="Pdf/` +Num_SAE+`.pdf">`+ Liste_AC +`</a>`;
 
+    //Génère une liste des Ressources associées à la SAE
     let Liste_ressources = "";
     for (let ressource in sae.ressources) {
         Liste_ressources += `<div>${ressource} : ${sae.ressources[ressource]}</div>`;
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,10 @@
+//Récupération des clés sous forme de tableau
 let Num_SAE=Object.keys(SAE);
 
-let content_HTML =""
+//Initialisation d'une chaine de caractère vide qui va accueillir les informations 
+let content_HTML ="";
 
+// boucle for pour générer la structure HTML et les liens
  for (let i = 0; i<Num_SAE.length; i++) 
     {  
      content_HTML += 
@@ -13,20 +16,27 @@ let content_HTML =""
          </div>
       </a>`;
     }
-    document.querySelector(".content").innerHTML += content_HTML
+//Ajout du contenu HTML dans la chaine de caractère initialiser plus haut 
+document.querySelector(".content").innerHTML += content_HTML
 
 console.log(Num_SAE)
+
+//Ajoute dans la variables boites les éléments de la classe cases  
 const boites = document.querySelectorAll(".cases");
 
+//Parcourt chaque "case" pour y ajouter les informations des SAE
 boites.forEach(function(cases, indice){
+   //Récupère la clé correspondant à l'indice
    const cle = Num_SAE[indice];
+   //Récupère les données correspondant à la clé
    const sae = SAE[cle];
 
+   //Condition if pour vérifié que les données existent réellement avant d'insérer les données.
    if(sae){
+      //Insère les informations des SAE dans les classe correspondante
       cases.querySelector(".num_sae").innerHTML = cle;
       cases.querySelector(".titre_sae").innerHTML = sae["titre"];
       cases.querySelector(".competences_sae").innerHTML = sae["compétences"].join(" ");
    }
 
 });
-
